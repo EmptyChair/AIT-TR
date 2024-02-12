d615512a5815661f2ecf808a0efe2e4c1c798c8e
EmptyChair <romaswebex01@gmail.com>
2024-02-12 11:30:29 +0100

    Merge remote-tracking branch 'origin/master'

---
+++ src/lesson_012_Sorting/HA12_SelectionSorting.java
@@ -0,0 +1,30 @@
+package lesson_012_Sorting;
+
+import java.util.Arrays;
+
+public class HA12_SelectionSorting {
+    public static void main(String[] args) {
+        int[] data = {5,5,7,3,2};
+        System.out.println("We shall sort this array "+Arrays.toString(data));
+        System.out.println("We shall use Selection Sorting");
+
+        int temp;
+        int minIndex;
+        for (int i = 0; i< data.length; i++) {
+            minIndex = i;
+            for (int c = i+1; c<data.length; c++) {
+                if (data[c]<data[i]) {
+                    minIndex = c;
+                }
+            }
+            if (minIndex != i) {
+                //swap data[i] with data[minIndex]
+                temp = data[i];
+                data[i] = data[minIndex];
+                data[minIndex] = temp;
+            }
+            System.out.println("This is our array for iteration "+i+" of the selection sorting");
+            System.out.println(Arrays.toString(data));
+        }
+    }
+}
